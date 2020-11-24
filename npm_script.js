29a4c7bfee42bb66231867ed2af80459ef35883f
Niko Lockenvitz <nl@nikolockenvitz.de>
2020-11-24 19:01:54 +0100

    chore: open correct amo url when building beta version

--- npm_script.js
+++ npm_script.js
@@ -1,6 +1,7 @@
 const browserExtensionHelper = require("../browser-extension-helper");
 
 const AMO_URL = "https://addons.mozilla.org/en-US/developers/addon/61a504687c984937a292/versions/submit/";
+const AMO_URL_BETA = "https://addons.mozilla.org/en-US/developers/addon/4515c8ea15764ff8b4d1/versions/submit/";
 
 const ZIP_CONTENT = {
     folders: [
@@ -19,6 +20,7 @@ const README_BADGE_TEXT = `<a href="{URL_UPDATES}/{XPI_FILEPATH}">
 
 browserExtensionHelper.init({
     amoURL: AMO_URL,
+    amoURLbeta: AMO_URL_BETA,
     zipContent: ZIP_CONTENT,
     readmeBadgeText: README_BADGE_TEXT,
     zipFoldername: "zip",
