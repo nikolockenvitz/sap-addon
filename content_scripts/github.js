b94e3501f0a344add2f26435a568c5df52260886
Niko Lockenvitz <nl@nikolockenvitz.de>
2021-09-11 15:13:24 +0200

    github: fix auto sign-in for automatic logout w/o page reload

--- content_scripts/github.js
+++ content_scripts/github.js
@@ -41,10 +41,11 @@ const github = {
 };
 
 function signIn() {
-    if (url.pathname !== github.signIn.urlPath) return;
-    executeFunctionAfterPageLoaded(function () {
-        getSignInButtonAndClick();
-    });
+    if (url.pathname === github.signIn.urlPath) {
+        executeFunctionAfterPageLoaded(function () {
+            getSignInButtonAndClick();
+        });
+    }
 
     domObserver.registerCallbackFunction(github.signIn.optionName, function (mutations, _observer) {
         for (const { target } of mutations) {
@@ -52,24 +53,25 @@ function signIn() {
         }
     });
 }
-let alreadyFoundSignInButton = false;
+// the handler to click on the button will be invoked a lot (>30) -> setting a flag after the first click prevents this
+// but this needs to be time-bound, otherwise there will be no sign in after automatic logout
+let alreadyClickedSignButtonAt = 0;
+const DELAY_BEFORE_CLICKING_SIGNIN_BUTTON_AGAIN = 15 * 1000;
 function getSignInButtonAndClick(element) {
-    element = element || document;
+    element = element && element.querySelector ? element : document;
     try {
         const signInBtn = element.querySelector(github.signIn.query);
-        if (signInBtn && signInBtn.click && !alreadyFoundSignInButton) {
-            alreadyFoundSignInButton = true;
+        if (signInBtn && signInBtn.click && Date.now() - alreadyClickedSignButtonAt > DELAY_BEFORE_CLICKING_SIGNIN_BUTTON_AGAIN) {
+            alreadyClickedSignButtonAt = Date.now();
             setTimeout(function () {
                 signInBtn.click();
             }, 100); // when click is executed directly, github.tools.sap crashes in chrome
-            stopAutoSignIn();
         }
 
         // there's a redirect during login; in case redirects are blocked by the browser: click link
         const redirectLink = getSignInRedirectLink(element);
         if (redirectLink && redirectLink.click) {
             redirectLink.click();
-            stopAutoSignIn();
         }
     } catch {}
 }
